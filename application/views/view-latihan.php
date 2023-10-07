27719ed9782811143afced6beae2a65739fcc61c
xikeh <yayaarifmustofa@gmai.com>
2023-10-08 01:07:06 +0700

    menambah file mvc

---
+++ application/views/view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>latihan 1</title>
+  </head>
+  <body>
+    Haloo kawan.. Yuk kita belajar Web Programming..!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil;  ?>
+  </body>
+</html>
