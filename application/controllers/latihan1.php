6e04eaab899f4b06691c50e7ca6cf8e559499c78
xikeh <yayaarifmustofa@gmai.com>
2023-10-08 02:01:06 +0700

    modified indentasi

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -1,13 +1,10 @@
 <?php
-  class latihan1 extends CI_Controller
-  {
-    public function index()
-    {
+  class latihan1 extends CI_Controller{
+    public function index(){
       echo "Selamat Datang.. selamat belajar web programming";
     }
 
-    public function penjumlahan($n1, $n2)
-    {
+    public function penjumlahan($n1, $n2){
       $this->load->model('model_latihan1');
       $data['nilai1'] = $n1;
       $data['nilai2'] = $n2;
